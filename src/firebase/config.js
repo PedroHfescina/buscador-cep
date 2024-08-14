3432173112453cd31ce931cf2c264aa83a5b0f2e
PedroHfescina <pedrohenriquefescina@gmail.com>
2024-08-14 20:52:20 -0300

    project

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBlm2_FwOMmZqoJHb5m6IbHWvmNSWLt_zs",
+  authDomain: "buscador-de-cep-f4276.firebaseapp.com",
+  projectId: "buscador-de-cep-f4276",
+  storageBucket: "buscador-de-cep-f4276.appspot.com",
+  messagingSenderId: "394551721866",
+  appId: "1:394551721866:web:0d67659bedf7efe08ad273"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
